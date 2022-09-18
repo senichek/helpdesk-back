e1a085720216e0a9bff082447b29f74cdea5ab02
senichek <senichek@gmail.com>
2022-09-18 22:05:00 +0200

    Join room

--- chatServer/index.js
+++ chatServer/index.js
@@ -27,6 +27,7 @@ io.use((socket, next) => {
 
 // Get the list of all connected clients (users)
 io.on("connection", (socket) => {
+  console.log("Established chat connection")
   const users = [];
   for (let [id, socket] of io.of("/").sockets) {
     users.push({
@@ -38,6 +39,22 @@ io.on("connection", (socket) => {
 
   // Emit event to all connected clients except the sender
   socket.broadcast.emit("connected_chat_users", users);
+
+  socket.on('join_room', (room) => {
+    socket.join(room);
+    console.log(`Joined room ${room}`)
+  });
+
+  // Send messages to specific room (private message)
+  socket.on('send_msg', (message, room) => {
+    socket.to(room).emit('receive_msg', message);
+    console.log(`Message: ${message}, room: ${room}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Socket.IO client disconnected.');
+  });
+
 });
 
 module.exports = server;

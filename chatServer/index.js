41e1a0d4c14a752490ec7313fe252664a6383045
senichek <senichek@gmail.com>
2022-11-02 15:04:26 +0100

    Added url to cors of chat server

--- chatServer/index.js
+++ chatServer/index.js
@@ -10,7 +10,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    //origin: "http://localhost:3000",
+    origin: "https://help-desker.herokuapp.com",
     methods: ["GET", "POST"],
   },
 });

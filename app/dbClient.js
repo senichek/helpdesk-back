ce832a0fc1c7775c06e22e90ba3643e7a3138298
senichek <senichek@gmail.com>
2022-11-02 14:46:42 +0100

    heroku config

--- app/dbClient.js
+++ app/dbClient.js
@@ -5,13 +5,14 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: "localhost",
+    //host: "localhost",
+    host: "ec2-34-241-90-235.eu-west-1.compute.amazonaws.com",
     dialect: "postgres",
     dialectOptions: {
-      /* ssl: {
+      ssl: {
         require: true,
         rejectUnauthorized: false
-      } */
+      }
     },
     define: {
       updatedAt: "updated_at",
